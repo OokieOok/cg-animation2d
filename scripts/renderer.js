5b6fb1e75ca569e4d0261fdf927881675feb9160
Jackson <meye4606@stthomas.edu>
2023-03-09 15:40:48 -0600

    Add two more polygons to slide 1, one more to slide 2

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -12,9 +12,12 @@ class Renderer {
         this.fps = fps;
         this.start_time = null;
         this.prev_time = null;
-        this.theta = 0;
-        this.scalar = 0;
-        this.scale_scen = 1;
+        this.theta1 = 0;
+        this.theta2 = 0;
+        this.theta3 = 0;
+        this.scalar1 = 0;
+        this.scalar2x = 1;
+        this.scalar2y = 1;
     }
 
     // flag:  bool
@@ -70,27 +73,13 @@ class Renderer {
     updateTransforms(time, delta_time) {
         // TODO: update any transformations needed for animation
         if (this.slide_idx == 1) {
-            this.theta = Math.round((this.theta + 180 * (delta_time/1000)) % 360);
-            console.log(this.theta);
+            this.theta1 = Math.round((this.theta1 - 270 * (delta_time/1000)) % 360);
+            this.theta2 = Math.round((this.theta2 + 480 * (delta_time/1000)) % 360);
+            this.theta3 = Math.round((this.theta3 + 90 * (delta_time/1000)) % 360);
         } else if (this.slide_idx == 2) {
-            /*
-            if (this.scale_scen == 1) {
-                this.scalar = 1;
-                this.scale_scen = 2;
-            } else if (this.scale_scen == 2) {
-                this.scalar = 2;
-                this.scale_scen = 3;
-            } else if (this.scale_scen == 3) {
-                this.scalar = 1;
-                this.scale_scen = 4;
-            } else {
-                this.scalar = 0.5;
-                this.scale_scen = 1;
-            }
-            */
-
-            this.scalar = (this.scalar + 2 * (delta_time/1000)) % 2;
-
+            this.scalar1 = (this.scalar1 + 2 * (delta_time/1000)) % 2;
+            this.scalar2x = (this.scalar2x + 2 * (delta_time/1000)) % 2;
+            this.scalar2y = (this.scalar2y + 2 * (delta_time/1000)) % 2;
         }
     }
     
@@ -135,6 +124,8 @@ class Renderer {
     drawSlide1() {
         // TODO: draw at least 3 polygons that spin about their own centers
         //   - have each polygon spin at a different speed / direction
+
+        // First polygon
         let diamond = [
             Vector3(400, 150, 1),
             Vector3(500, 300, 1),
@@ -142,25 +133,66 @@ class Renderer {
             Vector3(300, 300, 1)
         ];
 
-        let tran_origin = new Matrix(3, 3);
-        mat3x3Translate(tran_origin, -400, -300);
-        console.log(tran_origin)
-
-        let rot_mat = new Matrix(3, 3);
-        mat3x3Rotate(rot_mat, this.theta);
-
-        let tran_back = new Matrix(3, 3);
-        mat3x3Translate(tran_back, 400, 300);
-
-        let final_mat = tran_back.mult(rot_mat);
-        final_mat = final_mat.mult(tran_origin);
-
+        let tran_origin1 = new Matrix(3, 3);
+        mat3x3Translate(tran_origin1, -400, -300);
+        console.log(tran_origin1)
+        let rot_mat1 = new Matrix(3, 3);
+        mat3x3Rotate(rot_mat1, this.theta1);
+        let tran_back1 = new Matrix(3, 3);
+        mat3x3Translate(tran_back1, 400, 300);
+        let final_mat1 = tran_back1.mult(rot_mat1);
+        final_mat1 = final_mat1.mult(tran_origin1);
 
         for (let i=0; i<diamond.length; i++) {
-            diamond[i] = final_mat.mult(diamond[i]);
+            diamond[i] = final_mat1.mult(diamond[i]);
         }
-
         this.drawConvexPolygon(diamond, [255,0,0,255]);
+
+        // Second polygon
+        let poly2 = [
+            Vector3(125, 225, 1),
+            Vector3(200, 300, 1),
+            Vector3(125, 375, 1),
+            Vector3(50, 300, 1)
+        ];
+
+        let tran_origin2 = new Matrix(3, 3);
+        mat3x3Translate(tran_origin2, -125, -300);
+        console.log(tran_origin2)
+        let rot_mat2 = new Matrix(3, 3);
+        mat3x3Rotate(rot_mat2, this.theta2);
+        let tran_back2 = new Matrix(3, 3);
+        mat3x3Translate(tran_back2, 125, 300);
+        let final_mat2 = tran_back2.mult(rot_mat2);
+        final_mat2 = final_mat2.mult(tran_origin2);
+
+        for (let i=0; i<poly2.length; i++) {
+            poly2[i] = final_mat2.mult(poly2[i]);
+        }
+        this.drawConvexPolygon(poly2, [0,255,0,255]);
+
+        // Third polygon
+        let poly3 = [
+            Vector3(675, 225, 1),
+            Vector3(750, 300, 1),
+            Vector3(675, 375, 1),
+            Vector3(600, 300, 1)
+        ];
+
+        let tran_origin3 = new Matrix(3, 3);
+        mat3x3Translate(tran_origin3, -675, -300);
+        console.log(tran_origin3)
+        let rot_mat3 = new Matrix(3, 3);
+        mat3x3Rotate(rot_mat3, this.theta3);
+        let tran_back3 = new Matrix(3, 3);
+        mat3x3Translate(tran_back3, 675, 300);
+        let final_mat3 = tran_back3.mult(rot_mat3);
+        final_mat3 = final_mat3.mult(tran_origin3);
+
+        for (let i=0; i<poly3.length; i++) {
+            poly3[i] = final_mat3.mult(poly3[i]);
+        }
+        this.drawConvexPolygon(poly3, [0,0,255,255]);
         
     }
 
@@ -170,30 +202,49 @@ class Renderer {
         //   - have each polygon grow / shrink different sizes
         //   - try at least 1 polygon that grows / shrinks non-uniformly in the x and y directions
 
+        // Polygon 1
         let diamond = [
-            Vector3(400, 150, 1),
-            Vector3(500, 300, 1),
-            Vector3(400, 450, 1),
-            Vector3(300, 300, 1)
+            Vector3(200, 150, 1),
+            Vector3(300, 300, 1),
+            Vector3(200, 450, 1),
+            Vector3(100, 300, 1)
         ];
 
-        let tran_origin = new Matrix(3, 3);
-        mat3x3Translate(tran_origin, -400, -300);
-
-        let scale_mat = new Matrix(3, 3);
-        mat3x3Scale(scale_mat, this.scalar, this.scalar);
-
-        let tran_back = new Matrix(3, 3);
-        mat3x3Translate(tran_back, 400, 300);
-
-        let final_mat = tran_back.mult(scale_mat);
-        final_mat = final_mat.mult(tran_origin);
+        let tran_origin1 = new Matrix(3, 3);
+        mat3x3Translate(tran_origin1, -250, -300);
+        let scale_mat1 = new Matrix(3, 3);
+        mat3x3Scale(scale_mat1, this.scalar1, this.scalar1);
+        let tran_back1 = new Matrix(3, 3);
+        mat3x3Translate(tran_back1, 250, 300);
+        let final_mat1 = tran_back1.mult(scale_mat1);
+        final_mat1 = final_mat1.mult(tran_origin1);
 
         for (let i=0; i<diamond.length; i++) {
-            diamond[i] = final_mat.mult(diamond[i]);
+            diamond[i] = final_mat1.mult(diamond[i]);
         }
-
         this.drawConvexPolygon(diamond, [255,0,0,255]);
+
+        // Polygon 2
+        let poly2 = [
+            Vector3(600, 150, 1),
+            Vector3(700, 300, 1),
+            Vector3(600, 450, 1),
+            Vector3(500, 300, 1)
+        ];
+
+        let tran_origin2 = new Matrix(3, 3);
+        mat3x3Translate(tran_origin2, -600, -300);
+        let scale_mat2 = new Matrix(3, 3);
+        mat3x3Scale(scale_mat2, this.scalar2x, this.scalar2y);
+        let tran_back2 = new Matrix(3, 3);
+        mat3x3Translate(tran_back2, 600, 300);
+        let final_mat2 = tran_back2.mult(scale_mat2);
+        final_mat2 = final_mat2.mult(tran_origin2);
+
+        for (let i=0; i<poly2.length; i++) {
+            poly2[i] = final_mat2.mult(poly2[i]);
+        }
+        this.drawConvexPolygon(poly2, [0,255,0,255]);
 
     }
 
